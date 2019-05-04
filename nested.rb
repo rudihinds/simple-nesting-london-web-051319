086d2c7e4eafa8713268854cd525690cb0d7fd3a
rudi hinds <rudihinds@gmail.com>
2019-05-04 11:47:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -65,6 +65,8 @@ def dennis_ritchies_language
     
 end
 
+programmer_hash[:dennis_ritchie][:languages]
+
 def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
