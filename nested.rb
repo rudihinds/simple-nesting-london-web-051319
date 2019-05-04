5f1094acd2698e9b05a72d095b4c0b82aca9e489
rudi hinds <rudihinds@gmail.com>
2019-05-04 12:50:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,11 +61,11 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     
+ programmer_hash[:dennis_ritchie][:languages =>]    
     
 end
 
-programmer_hash[:dennis_ritchie][:languages =>]
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
