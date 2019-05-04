84abbc999e36fbcee48deea52e7d8297c354a8a9
rudi hinds <rudihinds@gmail.com>
2019-05-04 13:23:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -101,7 +101,7 @@ end
 
 def changing_alan
   
-  programmer_hash[:alan_kay][:known_for] = "GUI"
+ 
   
 	programmer_hash = 
  		{
@@ -124,7 +124,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+      programmer_hash[:alan_kay][:known_for] = "GUI"
      
      
 end
